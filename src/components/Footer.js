2b1b43199a7f499bc3f5914c91e23c6e1ae801cd
KayKay-git <68304549+KayKay-git@users.noreply.github.com>
2021-05-11 00:45:07 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,13 +7,6 @@ const FooterPage = () => {
     <div class="footer">
     <p>&copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> Kalki </a></p>
     </div>
-    // <MDBFooter color="light-yellow" className="font-small pt-4 mt-4 ">
-    //   <div className="footer-copyright text-center py-3">
-    //     <MDBContainer fluid color="pink">
-    //       &copy; {new Date().getFullYear()} Copyright: <a href="https://www.mdbootstrap.com"> Kalki </a>
-    //     </MDBContainer>
-    //   </div>
-    // </MDBFooter>
   );
 }
 
